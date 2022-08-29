bd254b02bc121f17ced623255bcacd51414ab4c5
Thomas Thorin <thomas.thorin@gmail.com>
2022-08-29 13:47:42 +0200

    Add project files.

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿namespace MVCDemo.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+using System.Diagnostics;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult Name(string name)
+    {
+        ViewData["Hello"]="Hello "+name;
+        return View("Index");
+    }
+}
